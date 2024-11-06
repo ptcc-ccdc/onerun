bc037704981f5bace44841a525ff81ddfb2036c7
Jpb <jpb@myappserver.cloud>
2024-11-06 06:44:14 +0000

    sakjbsd

--- Scripts/onerun/dependencies/ubunutu.sh
+++ Scripts/onerun/dependencies/ubunutu.sh
@@ -14,20 +14,18 @@ clear
 
 # Update and install necessary PHP packages
 sudo apt-get update
-sudo apt-get install -y php php7.2-bcmath php7.2-ctype php7.2-curl php7.2-gd php7.2-json php7.2-mbstring php7.2-mysqli php7.2-pdo php7.2-zip php7.2-xml
+sudo apt-get install -y unzip php php7.2-bcmath php7.2-ctype php7.2-curl php7.2-gd php7.2-json php7.2-mbstring php7.2-mysqli php7.2-pdo php7.2-zip php7.2-xml
 
 # Backup the MySQL configuration file
 sudo cp /etc/mysql/my.cnf /etc/mysql/my.cnf.bak
 
 # Update the bind address in the MySQL configuration
 
-
-echo "[mysqld]" > /etc/mysql/my.cnf
-echo "bind-address = 127.0.0.1" >> /etc/mysql/my.cnf
+echo "[mysqld]" >/etc/mysql/my.cnf
+echo "bind-address = 127.0.0.1" >>/etc/mysql/my.cnf
 # Prompt the user to verify the bind address
 read -p "Verify 'bind-address = 127.0.0.1' is set in /etc/mysql/my.cnf (Press Enter to continue)"
 sudo nano /etc/mysql/my.cnf
-
 
 clear
 
@@ -45,20 +43,44 @@ read -p
 
 clear
 
-# Backup existing web directory and download ZenCart
-mkdir /srv/zen-cart-backup
+echo "Creating backup of the existing web directory..."
+mkdir -p /srv/zen-cart-backup
 cp -ra /var/www/html /srv/zen-cart-backup
+echo "Backup created at /srv/zen-cart-backup"
+mv /var/www/html /var/www/html-old
+mkdir /var/www/html
+
+echo "Downloading ZenCart..."
 wget -O /var/www/html/zen-cart.zip https://github.com/zencart/zencart/archive/refs/tags/v1.5.6.zip
 unzip /var/www/html/zen-cart.zip -d /var/www/html/zen-cart
+echo "ZenCart downloaded and extracted."
 
-# Prompt the user to edit the Apache configuration
-read -p "Check what user apache is running as change it if you need to it should be www-data"
-nano /etc/apache2/envvars
-mv /etc/apache2/sites-enabled/openshop.conf /etc/apache2/sites-enabled/openshop.conf.back 
-cp dependencies/www-conf/openshop.conf /etc/apache2/sites-enabled/openshop.conf
-echo "Now you have to edit /etc/apache2/sites-enabled/openshop.conf and change 'var/www/html/openshop' to 'zen-cart'. Press Enter to continue."
-read -p
-sudo nano /etc/apache2/sites-enabled/openshop.conf
+read -p "Check what user apache is running as (should be www-data). Press Enter to continue..."
+sudo nano /etc/apache2/envvars
+
+echo "Backing up existing openshop.conf..."
+sudo mv /etc/apache2/sites-enabled/openshop.conf /etc/apache2/sites-enabled/openshop.conf.bak
+
+
+
+echo "Disabling old site configurations..."
+sudo a2dissite openshop.conf
+sudo a2dissite 000-default.conf
+
+echo "Copying new configuration file..."
+sudo cp dependencies/www-conf/openshop.conf /etc/apache2/sites-available/openshop.conf
+
+read -p "Now edit /etc/apache2/sites-available/openshop.conf and change 'var/www/html/openshop' to 'var/www/html/zen-cart'. Press Enter to continue..."
+sudo nano /etc/apache2/sites-available/openshop.conf
+
+echo "Enabling the new openshop.conf..."
+sudo a2ensite openshop.conf
+
+echo "Checking Apache configuration..."
+sudo apache2ctl configtest
+
+read -p "Pause"
+
 
 clear
 sudo chown -R www-data:www-data /var/www/
@@ -70,10 +92,10 @@ sudo find /var/www/html/zen-cart -type d -exec chmod 755 {} \;
 sudo find /var/www/html/zen-cart -type f -exec chmod 644 {} \;
 
 # Restart Apache service
+
 echo "Restarting Apache2..."
 sudo systemctl restart apache2
 
-# Provide instructions for accessing the ZenCart installer
 echo "If everything went well, go to http://172.20.242.10/zen-cart/zc_install"
 
 # Update UFW firewall rules
